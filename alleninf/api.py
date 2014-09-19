1e03be2a1dca74e345428a412da4772d23b7f9a5
Chris Filo Gorgolewski <chris.gorgolewski@gmail.com>
2014-09-19 12:51:04 -0700

    Add option to exclude probes

--- alleninf/api.py
+++ alleninf/api.py
@@ -8,28 +8,30 @@ from alleninf.datasets import fetch_microarray_expression
 
 api_url = "http://api.brain-map.org/api/v2/data/query.json"
 
+
 def get_probes_from_genes(gene_names):
-    if not isinstance(gene_names,list):
+    if not isinstance(gene_names, list):
         gene_names = [gene_names]
-    #in case there are white spaces in gene names
-    gene_names = ["'%s'"%gene_name for gene_name in gene_names]
-    
+    # in case there are white spaces in gene names
+    gene_names = ["'%s'" % gene_name for gene_name in gene_names]
+
     api_query = "?criteria=model::Probe"
     api_query += ",rma::criteria,[probe_type$eq'DNA']"
     api_query += ",products[abbreviation$eq'HumanMA']"
-    api_query += ",gene[acronym$eq%s]"%(','.join(gene_names))
+    api_query += ",gene[acronym$eq%s]" % (','.join(gene_names))
     api_query += ",rma::options[only$eq'probes.id','name']"
-    
+
     data = json.load(urllib2.urlopen(api_url + api_query))
-    
+
     d = {probe['id']: probe['name'] for probe in data['msg']}
-    
+
     if not d:
-        raise Exception("Could not find any probes for %s gene. Check " \
-        "http://help.brain-map.org/download/attachments/2818165/HBA_ISH_GeneList.pdf?version=1&modificationDate=1348783035873 " \
-        "for list of available genes."%gene_name)
-    
+        raise Exception("Could not find any probes for %s gene. Check "
+                        "http://help.brain-map.org/download/attachments/2818165/HBA_ISH_GeneList.pdf?version=1&modificationDate=1348783035873 "
+                        "for list of available genes." % gene_name)
+
     return d
+
 
 def get_expression_values_from_probe_ids(probe_ids, restapi=True):
     if restapi:
@@ -37,53 +39,61 @@ def get_expression_values_from_probe_ids(probe_ids, restapi=True):
     else:
         return get_expression_values_from_probe_ids_hdf(probe_ids)
 
+
 def get_expression_values_from_probe_ids_restapi(probe_ids):
-    if not isinstance(probe_ids,list):
+    if not isinstance(probe_ids, list):
         probe_ids = [probe_ids]
-    #in case there are white spaces in gene names
-    probe_ids = ["'%s'"%probe_id for probe_id in probe_ids]
-    
-    api_query = "?criteria=service::human_microarray_expression[probes$in%s]"%(','.join(probe_ids))
+    # in case there are white spaces in gene names
+    probe_ids = ["'%s'" % probe_id for probe_id in probe_ids]
+
+    api_query = "?criteria=service::human_microarray_expression[probes$in%s]" % (
+        ','.join(probe_ids))
     data = json.load(urllib2.urlopen(api_url + api_query))
-    
-    expression_values = [[float(expression_value) for expression_value in data["msg"]["probes"][i]["expression_level"]] for i in range(len(probe_ids))]
+
+    expression_values = [[float(expression_value) for expression_value in data[
+        "msg"]["probes"][i]["expression_level"]] for i in range(len(probe_ids))]
     well_ids = [sample["sample"]["well"] for sample in data["msg"]["samples"]]
-    donor_names = [sample["donor"]["name"] for sample in data["msg"]["samples"]]
-    well_coordinates = [sample["sample"]["mri"] for sample in data["msg"]["samples"]]
-    
+    donor_names = [sample["donor"]["name"]
+                   for sample in data["msg"]["samples"]]
+    well_coordinates = [sample["sample"]["mri"]
+                        for sample in data["msg"]["samples"]]
+
     return expression_values, well_ids, donor_names
+
 
 def get_expression_values_from_probe_ids_hdf(probe_ids):
     files = fetch_microarray_expression()
     hdf_file = files.microarray_expression
-   
+
     h_handle = open_file(hdf_file, "r")
     donors = [g._v_name for g in list(h_handle.walkGroups("/"))[1:]]
     h_handle.close()
-    
-    probe_ids = ["'%s'"%probe_id for probe_id in probe_ids]
-    where_query = "index in [%s]"%(",".join(probe_ids))
-    
+
+    probe_ids = ["'%s'" % probe_id for probe_id in probe_ids]
+    where_query = "index in [%s]" % (",".join(probe_ids))
+
     expression_values = []
     well_ids = []
     donor_names = []
     for donor in donors:
-        df = pd.read_hdf(hdf_file, donor, where=where_query,close=True)
+        df = pd.read_hdf(hdf_file, donor, where=where_query, close=True)
         well_ids += [int(col[len("well_id_"):]) for col in df.columns]
-        donor_names += [donor,]*len(df.columns)
+        donor_names += [donor, ] * len(df.columns)
         expression_values.append(np.array(df))
-    
-    expression_values = list(np.concatenate(expression_values,axis=1))
+
+    expression_values = list(np.concatenate(expression_values, axis=1))
     return expression_values, well_ids, donor_names
+
 
 def get_mni_coordinates_from_wells(well_ids):
     package_directory = os.path.dirname(os.path.abspath(__file__))
-    frame = pd.read_csv(os.path.join(package_directory, "data", "corrected_mni_coordinates.csv"), header=0, index_col=0)
-    
+    frame = pd.read_csv(os.path.join(
+        package_directory, "data", "corrected_mni_coordinates.csv"), header=0, index_col=0)
+
     return list(frame.ix[well_ids].itertuples(index=False))
-    
+
 if __name__ == '__main__':
     probes_dict = get_probes_from_genes("HTR1A")
-    expression_values, well_ids, donor_names = get_expression_values_from_probe_ids_hdf(probes_dict.keys())
+    expression_values, well_ids, donor_names = get_expression_values_from_probe_ids_hdf(
+        probes_dict.keys())
     print get_mni_coordinates_from_wells(well_ids)
-    
--- alleninf/scripts.py
+++ alleninf/scripts.py
@@ -1,5 +1,6 @@
-#!/usr/bin/env python 
-import argparse, os
+#!/usr/bin/env python
+import argparse
+import os
 import numpy as np
 import pandas as pd
 import nibabel as nb
@@ -9,6 +10,7 @@ from alleninf.api import get_probes_from_genes,\
 from alleninf.data import get_values_at_locations, combine_expression_values
 from alleninf.analysis import fixed_effects, approximate_random_effects,\
     bayesian_random_effects
+
 
 def nifti_file(string):
     if not os.path.exists(string):
@@ -27,13 +29,16 @@ def nifti_file(string):
             raise argparse.ArgumentTypeError(msg)
     return string
 
+
 def main():
-    parser = argparse.ArgumentParser(description="Compare a statistical map with gene expression patterns from Allen Human Brain Atlas.")
-    parser.add_argument("stat_map", help="Unthresholded statistical map in the form of a 3D NIFTI file (.nii or .nii.gz) in MNI space.", type=nifti_file)
-    parser.add_argument("gene_name", help="Name of the gene you want to compare your map with. For list of all available genes see: " \
+    parser = argparse.ArgumentParser(
+        description="Compare a statistical map with gene expression patterns from Allen Human Brain Atlas.")
+    parser.add_argument(
+        "stat_map", help="Unthresholded statistical map in the form of a 3D NIFTI file (.nii or .nii.gz) in MNI space.", type=nifti_file)
+    parser.add_argument("gene_name", help="Name of the gene you want to compare your map with. For list of all available genes see: "
                         "http://help.brain-map.org/download/attachments/2818165/HBA_ISH_GeneList.pdf?version=1&modificationDate=1348783035873.",
                         type=str)
-    parser.add_argument("--inference_method", help="Which model to use: fixed - fixed effects, approximate_random - approximate random effects (default), "\
+    parser.add_argument("--inference_method", help="Which model to use: fixed - fixed effects, approximate_random - approximate random effects (default), "
                         "bayesian_random - Bayesian hierarchical model (requires PyMC3).",
                         default="approximate_random")
     parser.add_argument("--n_samples", help="(Bayesian hierarchical model) Number of samples for MCMC model estimation (default 2000).",
@@ -42,53 +47,65 @@ def main():
                         default=500, type=float)
     parser.add_argument("--probes_reduction_method", help="How to combine multiple probes: average (default) or pca - use first principal component (requires scikit-learn).",
                         default="average")
-    parser.add_argument("--mask", help="Explicit mask for the analysis in the form of a 3D NIFTI file (.nii or .nii.gz) in the same space and " \
+    parser.add_argument("--mask", help="Explicit mask for the analysis in the form of a 3D NIFTI file (.nii or .nii.gz) in the same space and "
                         "dimensionality as the stat_map. If not specified an implicit mask (non zero and non NaN voxels) will be used.",
                         type=nifti_file)
     parser.add_argument("--radius", help="Radius in mm of of the sphere used to average statistical values at the location of each probe (default: 4mm).",
                         default=4, type=float)
-    
+    parser.add_argument("--probe_exclusion_keyword", help="If the probe name includes this string the probe will not be used.",
+                        type=str)
+
     args = parser.parse_args()
-    
-    print "Fetching probe ids for gene %s"%args.gene_name
+
+    print "Fetching probe ids for gene %s" % args.gene_name
     probes_dict = get_probes_from_genes(args.gene_name)
-    print "Found %s probes: %s"%(len(probes_dict), ", ".join(probes_dict.values()))
-    
-    print "Fetching expression values for probes %s"%(", ".join(probes_dict.values()))
-    expression_values, well_ids, _, donor_names = get_expression_values_from_probe_ids(probes_dict.keys())
-    print "Found data from %s wells sampled across %s donors"%(len(well_ids), len(set(donor_names)))
-    
+    print "Found %s probes: %s" % (len(probes_dict), ", ".join(probes_dict.values()))
+
+    if args.probe_exclusion_keyword:
+        probes_dict = {probe_id: probe_name for (probe_id, probe_name) in probes_dict.iteritems() if not args.probe_exclusion_keyword in probe_name}
+        print "Probes after applying exclusion cryterion: %s" % (", ".join(probes_dict.values()))
+
+    print "Fetching expression values for probes %s" % (", ".join(probes_dict.values()))
+    expression_values, well_ids, donor_names = get_expression_values_from_probe_ids(
+        probes_dict.keys())
+    print "Found data from %s wells sampled across %s donors" % (len(well_ids), len(set(donor_names)))
+
     print "Combining information from selected probes"
-    combined_expression_values = combine_expression_values(expression_values, method=args.probes_reduction_method)
-    
+    combined_expression_values = combine_expression_values(
+        expression_values, method=args.probes_reduction_method)
+
     print "Translating locations of the wells to MNI space"
     mni_coordinates = get_mni_coordinates_from_wells(well_ids)
-    
+
     print "Checking values of the provided NIFTI file at well locations"
-    nifti_values = get_values_at_locations(args.stat_map, mni_coordinates, mask_file=args.mask, radius=args.radius, verbose=True)
-        
-    #preparing the data frame
-    names = ["NIFTI values", "%s expression"%args.gene_name, "donor ID"]
-    data = pd.DataFrame(np.array([nifti_values, combined_expression_values, donor_names]).T, columns=names)
+    nifti_values = get_values_at_locations(
+        args.stat_map, mni_coordinates, mask_file=args.mask, radius=args.radius, verbose=True)
+
+    # preparing the data frame
+    names = ["NIFTI values", "%s expression" % args.gene_name, "donor ID"]
+    data = pd.DataFrame(np.array(
+        [nifti_values, combined_expression_values, donor_names]).T, columns=names)
     data = data.convert_objects(convert_numeric=True)
     len_before = len(data)
     data.dropna(axis=0, inplace=True)
     nans = len_before - len(data)
     if nans > 0:
-        print "%s wells fall outside of the mask"%nans
-    
+        print "%s wells fall outside of the mask" % nans
+
     if args.inference_method == "fixed":
         print "Performing fixed effect analysis"
-        fixed_effects(data, ["NIFTI values", "%s expression"%args.gene_name])
-    
+        fixed_effects(data, ["NIFTI values", "%s expression" % args.gene_name])
+
     if args.inference_method == "approximate_random":
         print "Performing approximate random effect analysis"
-        approximate_random_effects(data, ["NIFTI values", "%s expression"%args.gene_name], "donor ID")
-        
+        approximate_random_effects(
+            data, ["NIFTI values", "%s expression" % args.gene_name], "donor ID")
+
     if args.inference_method == "bayesian_random":
         print "Fitting Bayesian hierarchical model"
-        bayesian_random_effects(data, ["NIFTI values", "%s expression"%args.gene_name], "donor ID", args.n_samples, args.n_burnin)
-    
-    
+        bayesian_random_effects(
+            data, ["NIFTI values", "%s expression" % args.gene_name], "donor ID", args.n_samples, args.n_burnin)
+
+
 if __name__ == '__main__':
     main()
